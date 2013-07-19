fc20391915e4347f90ef00288a67971803ef8c4b
Peyman Toreini <ptoreini@gmail.com>
2013-07-19 11:42:55 +0200

    PHP to run TOS commands
    Completed php code to read from DB and run file!

--- wsnlab/php/BeaconSend.php
+++ wsnlab/php/BeaconSend.php
@@ -7,14 +7,22 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
+  
+// find the FileAddress field in DB for Actiity one which is Beacon  
 $result = mysqli_query($con,"SELECT FileAddress FROM file WHERE ActivityID='1'");
+if (!$result) {
+    die('Query failed: ' . mysql_error());
+}
+//mysqli_result::fetch_assoc -- mysqli_fetch_assoc — Fetch a result row as an associative array
+$row = mysqli_fetch_assoc($result);
+$File = $row['FileAddress'];  
 
-echo ($result);
 
-$File=("main.ihex");
+
 for($i=0;$i<3;$i++){
 
         echo exec("tos-bsl -c /dev/ttyUSB".$i." --telosb  -r -e -I -p".$File);
-}
+		
+		}
 
 ?>
